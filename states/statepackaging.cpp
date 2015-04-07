6fced382d20306c61b1adee295ebbd4ca86e3156
Yanguam Siliagim <Rara@mogara.org>
2015-04-07 08:36:06 +0800

    state Packaging

--- states/statepackaging.cpp
+++ states/statepackaging.cpp
@@ -1,6 +1,8 @@
 #include "statepackaging.h"
 
-NBStatePackaging::NBStatePackaging(QObject *parent) : NBState(parent)
+#include <QTimer>
+
+NBStatePackaging::NBStatePackaging(QObject *parent) : NBState(parent), m_7z(NULL), m_waitTimer(NULL), m_running(false), m_isError(false)
 {
 
 }
@@ -13,5 +15,98 @@ NBStatePackaging::~NBStatePackaging()
 
 void NBStatePackaging::run()
 {
+    if (m_running) {
+        emit error();
+        return;
+    }
 
+    m_running = true;
+    m_isError = false;
+
+    if (m_waitTimer != NULL) {
+        delete m_waitTimer;
+        m_waitTimer = NULL;
+    }
+
+    if (m_7z != NULL) {
+        if (m_7z->state() != QProcess::NotRunning)
+            m_7z->kill();
+        if (m_7z->state() == QProcess::NotRunning || m_7z->waitForFinished()) {
+
+        } else {
+            // log
+            m_isError = true;
+            emit fatal();
+            return;
+        }
+        delete m_7z;
+        m_7z = NULL;
+    }
+
+    m_7z = new QProcess;
+    m_7z->setProgram("7z");
+    m_7z->setArguments(QStringList() << "????"); // todo:global setting // todo:set the arguments of 7z
+
+    connect(m_7z, (void (QProcess::*)(int, QProcess::ExitStatus))(&QProcess::finished), this, &NBStatePackaging::processFinished);
+    connect(m_7z, (void (QProcess::*)(QProcess::ProcessError))(&QProcess::error), this, &NBStatePackaging::processError);
+
+    // start timer before the process start
+
+    m_waitTimer = new QTimer();
+    m_waitTimer->setSingleShot(true);
+    m_waitTimer->setInterval(1800000);
+    connect(m_waitTimer, &QTimer::timeout, this, &NBStatePackaging::timeout);
+
+    m_7z->start();
+}
+
+void NBStatePackaging::processError(QProcess::ProcessError)  // designed to handle error, temporily no effect
+{
+    m_isError = true;
+}
+
+void NBStatePackaging::processFinished(int exitCode, QProcess::ExitStatus exitStatus)
+{
+    QProcess *p = qobject_cast<QProcess *>(sender());
+
+    if (p == NULL) {
+        // log
+        m_isError = true;
+        emit fatal();
+        return;
+    }
+
+
+    if (exitStatus != QProcess::NormalExit || exitCode != 0) {
+        QString stdErr = QString::fromLocal8Bit(p->readAllStandardError());
+        Q_UNUSED(stdErr);
+        m_isError = true;
+        m_running = false;
+        //log
+        emit error();
+        return;
+    }
+
+    QString stdOut = QString::fromLocal8Bit(p->readAllStandardOutput());
+    QString stdErr = QString::fromLocal8Bit(p->readAllStandardError());
+
+    // log
+    Q_UNUSED(stdOut); // temp solution
+    Q_UNUSED(stdErr);
+
+    m_running = false;
+
+    if (!m_isError)
+        emit finished();
+}
+
+void NBStatePackaging::timeout()
+{
+    m_isError = true;
+    //m_running = false;
+
+    if (m_7z->state() != QProcess::NotRunning)
+        m_7z->kill();
+
+    emit error();
 }
--- states/statepackaging.h
+++ states/statepackaging.h
@@ -2,6 +2,9 @@
 #define NBSTATEPACKAGING_H
 
 #include "state.h"
+#include <QProcess>
+
+class QTimer;
 
 class NBStatePackaging : public NBState
 {
@@ -12,9 +15,16 @@ public:
 
     void run();
 
-signals:
-
 public slots:
+    void processFinished(int exitCode, QProcess::ExitStatus exitStatus);
+    void processError(QProcess::ProcessError error);
+    void timeout();
+
+private:
+    QProcess *m_7z;
+    QTimer *m_waitTimer;
+    bool m_running;
+    bool m_isError;
 };
 
 #endif // NBSTATEPACKAGING_H
