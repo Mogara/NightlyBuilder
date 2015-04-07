564497000c33fd6fef7cfe894e1a3d9280e0aea1
Yanguam Siliagim <Rara@mogara.org>
2015-04-07 13:04:30 +0800

    生成bot_version.cpp

--- states/stategeneratingbotversioncpp.cpp
+++ states/stategeneratingbotversioncpp.cpp
@@ -1,4 +1,9 @@
 #include "stategeneratingbotversioncpp.h"
+#include "globalconfig.h"
+
+#include <QDir>
+#include <QFile>
+#include <QDate>
 
 NBStateGeneratingBotVersionCpp::NBStateGeneratingBotVersionCpp(QObject *parent) : NBState(parent)
 {
@@ -13,5 +18,39 @@ NBStateGeneratingBotVersionCpp::~NBStateGeneratingBotVersionCpp()
 
 void NBStateGeneratingBotVersionCpp::run()
 {
+    QDir d(GlobalConfig::ProjectPath);
 
+    if (!d.exists()) {
+        emit fatal();
+        return;
+    }
+
+    if (!d.cd("src")) {
+        emit fatal();
+        return;
+    }
+
+    QString p = d.filePath("bot_version.cpb");
+    if (!QFile::exists(p)) {
+        emit fatal();
+        return;
+    }
+
+    QFile f(p);
+    f.open(QIODevice::ReadOnly);
+
+    QByteArray ba = f.readAll();
+    f.close();
+
+    ba.replace("%date", QDate::currentDate().toString("yyyyMMdd").toLatin1());
+
+    p.replace(".cpb", ".cpp");
+
+    QFile t(p);
+    t.open(QIODevice::WriteOnly | QIODevice::Truncate);
+
+    t.write(ba);
+    t.close();
+
+    emit finished();
 }
