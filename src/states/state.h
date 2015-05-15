a8c8f869f72ee6ae4485895b8aedfd751eee8a7b
Yanguam Siliagim <Rara@mogara.org>
2015-05-15 23:38:29 +0800

    虚析构函数

--- src/states/state.h
+++ src/states/state.h
@@ -8,7 +8,7 @@ class NBState : public QObject
     Q_OBJECT
 public:
     explicit NBState(QObject *parent = 0);
-    ~NBState();
+    virtual ~NBState();
 
     inline int retryTimes() const { return m_retryTimes; }
 
